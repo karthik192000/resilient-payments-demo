be3e385878275dfe62975238649e8db5f998b3f1
Karthik Bankapur <bankapurkarthik@gmail.com>
2025-10-19 16:00:23 +0530

    Added dependencies and main class

---
+++ src/main/java/com/resilient/payments/demo/ResilientPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.resilient.payments.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilientPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResilientPaymentsApplication.class, args);
+    }
+}
