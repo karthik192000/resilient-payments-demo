25d77ad4d5561af8e2780360e74ca432c2b5bfd5
Karthik Bankapur <bankapurkarthik@gmail.com>
2025-11-05 13:31:05 +0530

    Added logs

--- payments-api/src/main/java/com/resilient/payments/demo/interceptor/MDCFilter.java
+++ payments-api/src/main/java/com/resilient/payments/demo/interceptor/MDCFilter.java
@@ -6,12 +6,15 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
+@Slf4j
 public class MDCFilter extends OncePerRequestFilter {
 
   @Override
@@ -19,9 +22,11 @@ public class MDCFilter extends OncePerRequestFilter {
       HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
     try {
+      log.debug("MDCFilter.doFilterInternal called for URI: {}", request.getRequestURI());
       populateCorrelationId(request, response);
       populatePartnerId(request, response);
       filterChain.doFilter(request, response);
+      log.debug("MDCFilter.doFilterInternal completed for URI: {}", request.getRequestURI());
     } finally {
       MDC.clear();
     }
@@ -33,19 +38,23 @@ public class MDCFilter extends OncePerRequestFilter {
   }
 
   protected void populateCorrelationId(HttpServletRequest request, HttpServletResponse response) {
+    log.debug("MDCFilter.populateCorrelationId called");
     String correlationId = request.getHeader(CORRELATION_ID_HEADER);
     if (StringUtils.isBlank(correlationId)) {
       correlationId = CORRELATION_ID_PREFIX + System.currentTimeMillis();
     }
     MDC.put(CORRELATION_ID_LOG_KEY, correlationId);
     response.setHeader(CORRELATION_ID_HEADER, correlationId);
+    log.debug("MDCFilter.populateCorrelationId set correlationId: {}", correlationId);
   }
 
   protected void populatePartnerId(HttpServletRequest request, HttpServletResponse response) {
+    log.debug("MDCFilter.populatePartnerId called");
     String partnerId = request.getHeader(PARTNER_ID_HEADER);
     if (StringUtils.isNotBlank(partnerId)) {
       MDC.put(PARTNER_ID_LOG_KEY, partnerId);
     }
     response.setHeader(PARTNER_ID_HEADER, partnerId);
+    log.debug("MDCFilter.populatePartnerId set partnerId: {}", partnerId);
   }
 }
